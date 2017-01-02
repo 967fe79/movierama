f8e16ef17c0bc4ad398813ffca919cf0299b7791
Anonymous Throwaway <>
2017-01-02 13:40:39 +0000

    Extract mail delivery settings into ENV vars
    Depending on how the app will send email in production (Amazon SES,
    Sendgrid, internal SMTP, etc) this configuration will need to change.
    
    It may need additional authentication configuration, depending on the
    chosen delivery mechanism. It could be as simple as a host/port on
    the local network, a remote SMTP provider, or even local sendmail.
    
    I've chosen `address` and `port` to illustrate the pattern, but this
    will need to be updated before going into production.
    
    More detail on the mail configuration options, including example
    setups, here:
    
    http://guides.rubyonrails.org/action_mailer_basics.html#action-mailer-configuration

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,6 +16,10 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
   ActionMailer::Base.default :from => 'no-reply@movierama.dev'
+  ActionMailer::Base.smtp_settings = {
+    :address  => ENV.fetch('SMTP_ADDRESS'),
+    :port     => ENV.fetch('SMTP_PORT')
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,6 +60,10 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
   ActionMailer::Base.default :from => 'no-reply@movierama.com'
+  ActionMailer::Base.smtp_settings = {
+    :address  => ENV.fetch('SMTP_ADDRESS'),
+    :port     => ENV.fetch('SMTP_PORT')
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
